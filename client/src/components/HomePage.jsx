b885ff3052d53def3b9c977fd626dfeb874363e6
fossi13 <fossi13@users.noreply.github.com>
2024-06-12 23:12:18 +0700

    FIX: edit responsive and edit line 70 - 83 to show data tags

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-
 function Homepage() {
   const [travels, setDataTravel] = useState([]);
   const [findTravel, setFindTravel] = useState("");
@@ -11,6 +10,11 @@ function Homepage() {
       `http://localhost:4001/trips?keywords=${findTravel}`
     );
     setDataTravel(result.data.data);
+    console.log(result.data.data);
+  };
+
+  const handleClick = (findTravel) => {
+    setFindTravel(findTravel)
   };
 
   useEffect(() => {
@@ -21,53 +25,76 @@ function Homepage() {
     setFindTravel(event.target.value);
   };
 
-  const truncateDescription = (description) => {
+  function truncateDescription(description) {
     if (description.length <= 100) {
       return description;
     }
     return description.slice(0, 100) + "...";
-  };
+  }
 
   return (
     <section>
       <h1 className="texthead">เที่ยวไหนดี</h1>
       <h4>ค้นหาที่เที่ยว</h4>
-    <input
-          id="find-travel"
-          type="text"
-          value={findTravel}
-          onChange={handleTodoTravel}
-          placeholder="หาที่เที่ยวแล้วไปกัน ..."
-        />
+    <div className="text">
+      <input
+        id="find-travel"
+        type="text"
+        value={findTravel}
+        onChange={handleTodoTravel}
+        placeholder="หาที่เที่ยวแล้วไปกัน ..."
+      />
+    </div>
       
+
       <main>
         {travels.map((travel, index) => {
           return (
             <div className="travel-list" key={index}>
               <aside>
-                <img className="picture0" width={450} src={travel.photos[0]} alt="main-picture" />
+                <img
+                  className="picture0"
+                  width={400}
+                  height={320}
+                  src={travel.photos[0]}
+                  alt="main-picture"
+                />
               </aside>
               <body className="contanier">
                 <h3>{travel.title}</h3>
                 <p>{truncateDescription(travel.description)}</p>
-                
-                  <a href={travel.url} target="_blank">
-                    อ่านต่อ
-                  </a>
-                  
-                 <p>หมวด : <span className="tag"> {travel.tags.join(' ')}</span> </p>
-                 
-                
-                <div >
+
+                <a href={travel.url} target="_blank">
+                  อ่านต่อ
+                </a>
+                <p>
+                  หมวด :
+                  {travel.tags.map((tag, index) => {
+                    return (
+                      <span
+                        className="tag"
+                        key={index}
+                        onClick={() => handleClick(tag)}
+                      >
+                        {tag}
+                      </span>
+                    );
+                  })}
+                </p>
+
+                <div>
                   {travel.photos.slice(1).map((photo, index) => {
-                    return <img className="allpicture" width={200} key={index + 1} src={photo} />;
+                    return (
+                      <img
+                        className="allpicture"
+                        width={130}
+                        height={130}
+                        key={index + 1}
+                        src={photo}
+                      />
+                    );
                   })}
                 </div>
-
-                {/* <img  width={250} src={travel.photos[1]} alt="picture1" />
-          <img  width={250} src={travel.photos[2]} alt="picture2" />
-          <img  width={250} src={travel.photos[3]} alt="picture3" /> */}
-
               </body>
             </div>
           );
